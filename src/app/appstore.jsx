b9963112760ebd1f8dc99db68f7c8b236d8cc922
joakim.obstfelder@gmail.com <joakim.obstfelder@gmail.com>
2018-02-05 22:00:55 +0100

    Added data

--- src/app/appstore.jsx
+++ src/app/appstore.jsx
@@ -3,5 +3,8 @@ import {observable} from 'mobx'
 export default class AppStore {
 	@observable text = 'React + MobX + React Router + Material-UI + CSS Modules'
 	@observable projects = []
-    @observable projectsUrl = 'https://raw.githubusercontent.com/fatterpave/projectb/master/data/projects.json'
+    @observable projectsUrl = 'https://raw.githubusercontent.com/fatterpave/projectc/master/data/projects.json'
+    @observable contributorsUrl = 'https://raw.githubusercontent.com/fatterpave/projectc/master/data/contributors.json'
+    @observable projecttypesUrl = 'https://raw.githubusercontent.com/fatterpave/projectc/master/data/projecttypes.json'
+    @observable milestonetypesUrl = 'https://raw.githubusercontent.com/fatterpave/projectc/master/data/milestonetypes.json'
 }
